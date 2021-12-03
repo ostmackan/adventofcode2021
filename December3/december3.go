882de536ed91fd6d01390035b435962d34130e49
Niklas Pettersson <public@niklaspettersson.se>
2021-12-03 22:55:06 +0100

    Day 3 done, worst code i have ever done.

--- December3/december3.go
+++ December3/december3.go
@@ -6,14 +6,59 @@ import (
 	"strconv"
 )
 
+func ConvertTo(line []int) string {
+	var result string = ""
+	for i := range line {
+		result += fmt.Sprint(line[i])
+	}
+	return result
+}
+
+func MoreZeroesOrOnes(lines [][]int, i int) int {
+	var (
+		ones   int = 0
+		zeroes int = 0
+	)
+	for l := range lines {
+		if lines[l][i] == 1 {
+			ones++
+		} else {
+			zeroes++
+		}
+	}
+	if ones >= zeroes {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func Keep(lines [][]int, i int, keep int) [][]int {
+	var result [][]int
+
+	for l := range lines {
+		if lines[l][i] == keep {
+			result = append(result, lines[l])
+		}
+	}
+
+	return result
+}
+
 func main() {
 	var (
-		lines  [][]int
-		result []int
-		most   string = ""
-		least  string = ""
+		lines       [][]int
+		mostLines   [][]int
+		leastLines  [][]int
+		result      []int
+		most        string = ""
+		least       string = ""
+		mostResult  string
+		leastResult string
 	)
 	lines = fileIo.ReadAsIntegerArray("input.txt")
+	mostLines = fileIo.ReadAsIntegerArray("input.txt")
+	leastLines = fileIo.ReadAsIntegerArray("input.txt")
 
 	if len(lines) > 0 {
 		for i := range lines[0] {
@@ -34,6 +79,22 @@ func main() {
 				result = append(result, 0)
 			}
 
+			if len(mostLines) > 1 {
+				mostLinesZeroOrOne := MoreZeroesOrOnes(mostLines, i)
+				mostLines = Keep(mostLines, i, mostLinesZeroOrOne)
+			}
+			if len(leastLines) > 1 {
+				leastLinesZeroOrOne := MoreZeroesOrOnes(leastLines, i)
+				var leastResult = 0
+				if leastLinesZeroOrOne == 0 {
+					leastResult = 1
+				}
+				leastLines = Keep(leastLines, i, leastResult)
+			}
+
+			fmt.Printf("mostLines: %v\n", mostLines)
+			fmt.Printf("leastLines: %v\n", leastLines)
+
 		}
 		for i := range result {
 			most += fmt.Sprint(result[i])
@@ -42,21 +103,33 @@ func main() {
 			} else {
 				least += fmt.Sprint(0)
 			}
-
 		}
 
-		mostValue, err := strconv.ParseInt(most, 2, 16)
+		if len(leastLines) == 1 {
+			leastResult = ConvertTo(leastLines[0])
+		} else {
+			panic("leastLines more or less than 1")
+		}
+
+		if len(mostLines) == 1 {
+			mostResult = ConvertTo(mostLines[0])
+		} else {
+			panic("leastLines more or less than 1")
+		}
+
+		mostValue, err := strconv.ParseInt(mostResult, 2, 16)
 		if err != nil {
 			panic(err)
 		}
-		leastValue, err := strconv.ParseInt(least, 2, 16)
+		leastValue, err := strconv.ParseInt(leastResult, 2, 16)
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Printf("most: %s %d\n", most, mostValue)
-		fmt.Printf("most: %s %d\n", least, leastValue)
+		fmt.Printf("most: %s %d\n", mostResult, mostValue)
+		fmt.Printf("most: %s %d\n", leastResult, leastValue)
 		fmt.Println("answer: " + fmt.Sprint(mostValue*leastValue))
+
 	}
 
 }
