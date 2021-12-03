b3b2e0a91ee09322bdbdefa124fa02f878b88574
Niklas Pettersson <public@niklaspettersson.se>
2021-12-03 19:58:05 +0100

    Day2 part1

--- December2/december2.go
+++ December2/december2.go
@@ -3,9 +3,56 @@ package main
 import (
 	fileIo "aoc2021/december2/pkg/fileIO"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
+type subCommand struct {
+	direction string
+	units     int
+}
+
+func GetCommands(lines []string) []subCommand {
+	var subCommands []subCommand
+
+	for i := range lines {
+		s := strings.Split(lines[i], " ")
+		var cmd subCommand
+		if len(s) == 2 {
+			cmd.direction = s[0]
+			var units, err = strconv.Atoi(s[1])
+			if err != nil {
+				panic(err)
+			} else {
+				cmd.units = units
+				subCommands = append(subCommands, cmd)
+			}
+		}
+	}
+
+	return subCommands
+}
+
 func main() {
-	[]lines := fileIo.ReadLines("input.txt")
-	fmt.Print("hej")
+	var (
+		lines    []string
+		position int = 0
+		depth    int = 0
+	)
+	lines = fileIo.ReadLines("input.txt")
+	commands := GetCommands(lines)
+	for i := range commands {
+		command := commands[i]
+		switch command.direction {
+		case "forward":
+			position += command.units
+		case "up":
+			depth -= command.units
+		case "down":
+			depth += command.units
+		}
+	}
+	println("position: " + fmt.Sprint(position))
+	println("depth: " + fmt.Sprint(depth))
+	println("answer: " + fmt.Sprint(position*depth))
 }
--- December2/pkg/fileIO/fileIo.go
+++ December2/pkg/fileIO/fileIo.go
@@ -1,6 +1,23 @@
 package fileIo
 
-func ReadLines(filename string) []string {
+import (
+	"bufio"
+	"os"
+)
 
-	
+func ReadLines(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	} else {
+		scanner := bufio.NewScanner(file)
+		var lines []string
+		for scanner.Scan() {
+			var line = scanner.Text()
+			lines = append(lines, line)
+		}
+
+		return lines
+	}
+
 }
